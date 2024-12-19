ecbfed6cc2964c84403d4af4520054bb5e2ab1ff
fr0wiNN <gach.maksymilian@gmail.com>
2024-12-19 19:48:10 +0100

    Renamed computeTotalLoss method

--- src/main/java/com/maksymiliangach/ai/Regression/RegressionModel.java
+++ src/main/java/com/maksymiliangach/ai/Regression/RegressionModel.java
@@ -4,13 +4,13 @@ import com.maksymiliangach.ai.Model;
 
 import java.io.Serializable;
 
-public interface RegressionModel extends Model{
+public interface RegressionModel extends Model {
     void train(double[][] inputs, double[] outputs);
     double validate(double[][] testInputs, double[] testOutputs);
     double predict(double[] inputs);
     double[] forward(double[][] inputs);
     void backward(double[][] inputs, double[] outputs, double[] predictions);
-    double computeLoss(double[] yTrue, double[] yPredicted);
+    double computeTotalLoss(double[] yTrue, double[] yPredicted);
     double[] getWeights();
     double getBias();
 }
