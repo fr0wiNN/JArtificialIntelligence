4b1ad42bdac693ae0c465d44bde8222084c1f8a6
fr0wiNN <gach.maksymilian@gmail.com>
2024-12-19 19:45:57 +0100

    Changed to comply with new logging and builder system

--- src/main/java/com/maksymiliangach/ai/Main.java
+++ src/main/java/com/maksymiliangach/ai/Main.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 
 import com.maksymiliangach.ai.DataManager.JDataFrame;
 import com.maksymiliangach.ai.DataManager.JDataSplitter;
-import com.maksymiliangach.ai.Regression.*;
+import com.maksymiliangach.ai.Logger.JLogger;
 import com.maksymiliangach.ai.Plotter.LinearRegressionPlotter;
 import com.maksymiliangach.ai.Regression.LinearRegression.LinearRegression;
+import com.maksymiliangach.ai.Regression.LinearRegression.LinearRegressionBuilder;
 
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -30,55 +31,34 @@ public class Main {
         double[][] testingInputData = testingDF.getColumns(0, 1, 2);
         double[]   testingOutputData = testingDF.getColumn(3);
 
-        LinearRegression model = new LinearRegression(0.00000210, 30_000_000);
+        LinearRegression model = new LinearRegressionBuilder().setModelName("Real Estate Price Model")
+                                                              .setLearningRate(0.00000110)
+                                                              .setEpochs(30_000_000)
+                                                              .setLogger(JLogger.PROGRESS_BAR)
+                                                              .build();
 
-        // model.setLogging(true);
-
-        LinearRegressionPlotter plotter = new LinearRegressionPlotter(model);
-        plotter.setDisplayDataPlot(true)
-               .setDisplayLossPlot(true)
-               .setDataLabels("Linear Regression Visualization", "Square Feet", "Price")
-               .setLossLabels("Total Loss Visualization", "Epoch", "Loss Function Value");
-
+        LinearRegressionPlotter plotter = new LinearRegressionPlotter(model).setDisplayDataPlot(true).setDisplayLossPlot(true);
         model.setPlotter(plotter);
 
         model.train(trainingInputData, trainingOutputData);
 
         System.out.println(model.summary());
 
-        System.out.printf("Total Loss for Training Data: %f\n", model.validate(trainingInputData, trainingOutputData));
-        System.out.printf("Total Loss for Testing Data: %f\n", model.validate(testingInputData, testingOutputData));
-
-
-        // TODO create a method for getting better input array for training - think of good name for such function
-
-        // Convert X1 and X2 columns to training array
-        //double[][] x = JDataFrame.toTraining(X1, X2);
-        //double[][] x = df.getColumns(1, 9);
-
-        // Define LinearRegression model
         // TODO: create builder class for LinearRegression model creation (set hyper parameters: lr, modelName or load from config)
-        //LinearRegression model = new LinearRegressionExample(0.0000170, 1_000_000);
-        //model.setLogging(true);
+        String modelName = "model.lr";
+        if (model.save(modelName)) {
+            System.out.println("Successfully saved: " + modelName);
+        } else {
+            System.err.println("Error while saving: " + modelName);
+        }
 
-        // Define LinearRegressionPlotter for data visualisation
-        //LinearRegressionPlotter plotter = new LinearRegressionPlotter(model);
-        //plotter.setDisplayDataPlot(true)
-        //       .setDisplayLossPlot(true)
-        //       .setDataLabels("Real Estate Linear Regression Visualization", "Square Feet", "Price")
-        //       .setLossLabels("Total Loss Visualisation", "Epoch", "Loss Function Value");
+        LinearRegression loadedModel = LinearRegression.load(modelName);
+        loadedModel.setModelName("Real Estate Price Loaded Model");
 
-        // Set plotter
-        //model.setPlotter(plotter);
+        System.out.println(loadedModel.summary());
 
-        // Train model
-        //model.train(x, y);
-
-        // TODO: save model as model.lr or create a config file
-        // TODO: load model from model.lr config file
-
-        // Print summary
-        //System.out.println(model.summary());
+        System.out.printf("Total Loss for Training Data: %f\n", loadedModel.validate(trainingInputData, trainingOutputData));
+        System.out.printf("Total Loss for Testing Data: %f\n", loadedModel.validate(testingInputData, testingOutputData));
     }
 
 }
