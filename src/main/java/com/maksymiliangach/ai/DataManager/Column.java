234722e38dcc7d11c92dcd8c1fe55317eecd956f
fr0wiNN <gach.maksymilian@gmail.com>
2024-12-19 12:53:02 +0100

    Refactored the Column class by methods and field orders. Added size() method

--- src/main/java/com/maksymiliangach/ai/DataManager/Column.java
+++ src/main/java/com/maksymiliangach/ai/DataManager/Column.java
@@ -7,32 +7,48 @@ public class Column {
     private String name;
     private List<Double> data;
 
-    public Column(String name){
+    // Public constructor
+    public Column(String name) {
         this.name = name;
         this.data = new ArrayList<>();
     }
 
-    public String getName() { return  name; }
-    public void setName(String name) { this.name = name; }
-    public void add(double value) { this.data.add(value); }
-    public double getRow(int i) { return data.get(i); }
-    public void removeRow(int i) {data.remove(i);}
-    public double[] toArray() {
-        Double[] array = new Double[data.size()];
-        for(int x = 0; x<data.size() ; x++){
-            array[x] = data.get(x);
-        }
-        return java.util.Arrays.stream(array).mapToDouble(Double::doubleValue).toArray();
-    }
-
     // Copy constructor
-    private Column(Column original){
+    private Column(Column original) {
         this.name = original.name;
         this.data = new ArrayList<>(original.data);
     }
 
-    public Column copy(){
-        return new Column(this);
+    // Public methods
+    public String getName() {
+        return this.name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void add(double value) {
+        this.data.add(value);
+    }
+
+    public double getRow(int i) {
+        return this.data.get(i);
+    }
+
+    public void removeRow(int i) {
+        this.data.remove(i);
+    }
+
+    public int size() {
+        return this.data.size();
+    }
+
+    public double[] toArray() {
+        return data.stream().mapToDouble(Double::doubleValue).toArray();
+    }
+
+    public Column copy() {
+        return new Column(this);
+    }
 }
